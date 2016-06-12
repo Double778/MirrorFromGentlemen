31efc3e2cc2194906c1e04dabe3759a0124a2215
韩旭 <982810829@qq.com>
2016-06-12 20:23:53 +0800

    Commonly used tools

---
+++ MirrorFromGentleman/app/src/main/java/com/zhao/mirrorfromgentleman/ui/utils/usedtools/KeyBoardUtils.java
@@ -0,0 +1,46 @@
+package com.zhao.mirrorfromgentleman.ui.utils.usedtools;
+
+/**
+ * Created by hanxu on 16/6/12.
+ */
+
+import android.widget.EditText;
+
+
+import android.content.Context;
+import android.view.inputmethod.InputMethodManager;
+
+
+/**
+ * 打开或关闭软键盘
+ *
+ * @author zhy
+ */
+public class KeyBoardUtils {
+    /**
+     * 打卡软键盘
+     *
+     * @param mEditText 输入框
+     * @param mContext  上下文
+     */
+    public static void openKeybord(EditText mEditText, Context mContext) {
+        InputMethodManager imm = (InputMethodManager) mContext
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(mEditText, InputMethodManager.RESULT_SHOWN);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,
+                InputMethodManager.HIDE_IMPLICIT_ONLY);
+    }
+
+    /**
+     * 关闭软键盘
+     *
+     * @param mEditText 输入框
+     * @param mContext  上下文
+     */
+    public static void closeKeybord(EditText mEditText, Context mContext) {
+        InputMethodManager imm = (InputMethodManager) mContext
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        imm.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
+    }
+}

b93167add743b41552c430790a33964c60c65217
Double778 <596638119@qq.com>
2016-06-16 08:43:42 +0800

    UniversalImageLoader

--- MirrorFromGentleman/app/src/main/java/com/zhao/mirrorfromgentleman/ui/MyApplication.java
+++ MirrorFromGentleman/app/src/main/java/com/zhao/mirrorfromgentleman/ui/MyApplication.java
@@ -3,6 +3,9 @@ package com.zhao.mirrorfromgentleman.ui;
 import android.app.Application;
 import android.content.Context;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
 /**
  * Created by 华哥哥 on 16/6/12.
  * 自定义Application
@@ -15,6 +18,9 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
+        // UniversalImageLoader 配置
+        ImageLoaderConfiguration imageLoaderConfiguration = ImageLoaderConfiguration.createDefault(this);
+        ImageLoader.getInstance().init(imageLoaderConfiguration);
     }
 
     public static Context getContext() {

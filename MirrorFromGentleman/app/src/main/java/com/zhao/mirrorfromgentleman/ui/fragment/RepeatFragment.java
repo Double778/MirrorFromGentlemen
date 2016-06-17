e700d47ea3d304fe64915c56337c29c186af4502
韩旭 <982810829@qq.com>
2016-06-17 16:25:00 +0800

    推一下

--- MirrorFromGentleman/app/src/main/java/com/zhao/mirrorfromgentleman/ui/fragment/RepeatFragment.java
+++ MirrorFromGentleman/app/src/main/java/com/zhao/mirrorfromgentleman/ui/fragment/RepeatFragment.java
@@ -60,6 +60,8 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
     RecyclerView recyclerView;
     @BindView(R.id.img)
     ImageView iv;
+    @BindView(R.id.login_tv)
+    TextView loginTv;
     //pppLv的定义
     private ListView lv;
     private RepeatLvAdapter adapter;
@@ -67,9 +69,9 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
     private PopupWindow popupWindow;
     private List<MyData> mydata;
 
-    private TextView allTv, flatLightTv, sunglassesTv, shareTv, tv5, tv6, tv7;
+    private TextView allTv, flatLightTv, sunglassesTv, shareTv,  shoppingCartTv, returnTv, exitTv;
 
-    private ImageView iv1, iv2, iv3, iv4, iv5;
+    private ImageView allIv, flatLightIv, sunglassesIv, shareIv, shoppingCartIv;
 
 
     //定义这个内部接口
@@ -81,6 +83,7 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
     public RepeatFragment(String titles) {
         this.titles = titles;
     }
+
     //将set这个接口给出去
 //    public void setControlViewpager(ControlViewpager controlViewpager) {
 //        this.controlViewpager = controlViewpager;
@@ -96,11 +99,8 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
     @Override
     public void initData() {
 
-
-
         //点击出ppp的点击事件
         pop_up.setOnClickListener(this);
-
 
         //设置这个文字的改变
         titlesTv.setText(titles);
@@ -109,10 +109,8 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
             recyclerView.setVisibility(View.INVISIBLE);
             iv.setVisibility(View.VISIBLE);
 
-
-
         }
-
+        //这个ppp的调取
         initPupop();
 
 
@@ -120,8 +118,6 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
         Map<String, String> params = new HashMap<>();
         params.put("token", "0");
         params.put("device_type", "2");
-
-
         OkHttpClientManager.postAsyn("http://api101.test.mirroreye.cn/index.php/products/goods_list", new OkHttpClientManager.ResultCallback<TestBean>() {
             @Override
             public void onError(Request request, Exception e) {
@@ -154,6 +150,8 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
         });
     }
 
+
+    //通过点击事件控制这些东西显示和不显示
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -162,41 +160,35 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
                 pop_up.setVisibility(View.INVISIBLE);
                 recyclerView.setVisibility(View.INVISIBLE);
                 break;
-            case R.id.tv1:
+            case R.id.all_tv:
                 controlViewpager.control(0);
                 popupWindow.dismiss();
                 break;
-            case R.id.tv2:
+            case R.id.flat_light_tv:
                 controlViewpager.control(1);
                 popupWindow.dismiss();
                 break;
-            case R.id.tv3:
+            case R.id.sunglasses_tv:
                 controlViewpager.control(2);
                 popupWindow.dismiss();
                 break;
-            case R.id.tv4:
+            case R.id.share_tv:
                 controlViewpager.control(3);
                 popupWindow.dismiss();
                 break;
-            case R.id.tv5:
+            case R.id.shopping_cart_tv:
                 controlViewpager.control(4);
                 popupWindow.dismiss();
                 break;
-            case R.id.tv6:
+            case R.id.return_tv:
                 controlViewpager.control(0);
                 popupWindow.dismiss();
                 break;
-            case R.id.tv7:
+            case R.id.exit_tv:
                 clickQuitDialog();
                 popupWindow.dismiss();
                 break;
         }
-
-
-//        popupWindow.showAsDropDown(pop_up);
-//        pop_up.setVisibility(View.INVISIBLE);
-//        recyclerView.setVisibility(View.INVISIBLE);
-
 
     }
 
@@ -221,11 +213,9 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
         popupWindow.setOutsideTouchable(true);
 
         frm_ppp_Lt = LayoutInflater.from(getActivity()).inflate(R.layout.frm_repeat_ppp, null);
-        frm_ppp_Lt.startAnimation(animation1);
+//        frm_ppp_Lt.startAnimation(animation1);
 
-        initdatas();
-
-
+        //初始化这些个组件
         allTv = (TextView) frm_ppp_Lt.findViewById(R.id.all_tv);
         allTv.setOnClickListener(this);
         flatLightTv = (TextView) frm_ppp_Lt.findViewById(R.id.flat_light_tv);
@@ -234,69 +224,43 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
         sunglassesTv.setOnClickListener(this);
         shareTv = (TextView) frm_ppp_Lt.findViewById(R.id.share_tv);
         shareTv.setOnClickListener(this);
-        tv5 = (TextView) frm_ppp_Lt.findViewById(R.id.tv5);
-        tv5.setOnClickListener(this);
-        tv6 = (TextView) frm_ppp_Lt.findViewById(R.id.tv6);
-        tv6.setOnClickListener(this);
-        tv7 = (TextView) frm_ppp_Lt.findViewById(R.id.tv7);
-        tv7.setOnClickListener(this);
-        iv1 = (ImageView) frm_ppp_Lt.findViewById(R.id.iv1);
-        iv2 = (ImageView) frm_ppp_Lt.findViewById(R.id.iv2);
-        iv3 = (ImageView) frm_ppp_Lt.findViewById(R.id.iv3);
-        iv4 = (ImageView) frm_ppp_Lt.findViewById(R.id.iv4);
-        iv5 = (ImageView) frm_ppp_Lt.findViewById(R.id.iv5);
+        shoppingCartTv = (TextView) frm_ppp_Lt.findViewById(R.id.shopping_cart_tv);
+        shoppingCartTv.setOnClickListener(this);
+        returnTv = (TextView) frm_ppp_Lt.findViewById(R.id.return_tv);
+        returnTv.setOnClickListener(this);
+        exitTv = (TextView) frm_ppp_Lt.findViewById(R.id.exit_tv);
+        exitTv.setOnClickListener(this);
+        allIv = (ImageView) frm_ppp_Lt.findViewById(R.id.all_iv);
+        flatLightIv = (ImageView) frm_ppp_Lt.findViewById(R.id.flat_light_iv);
+        sunglassesIv = (ImageView) frm_ppp_Lt.findViewById(R.id.sunglasses_iv);
+        shareIv = (ImageView) frm_ppp_Lt.findViewById(R.id.share_iv);
+        shoppingCartIv = (ImageView) frm_ppp_Lt.findViewById(R.id.shopping_cart_iv);
 
-
+        //通过判断titles 来改变点击后的字体颜色
         switch (titles) {
             case "浏览所有分类":
-                tv1.setTextColor(getResources().getColor(R.color.colorAccent));
-                iv1.setVisibility(View.VISIBLE);
+                allTv.setTextColor(getResources().getColor(R.color.colorwhirt));
+                allIv.setVisibility(View.VISIBLE);
                 break;
             case "浏览平光镜":
-                tv2.setTextColor(getResources().getColor(R.color.colorAccent));
-                iv2.setVisibility(View.VISIBLE);
+                flatLightTv.setTextColor(getResources().getColor(R.color.colorwhirt));
+                flatLightIv.setVisibility(View.VISIBLE);
                 break;
             case "浏览太阳镜":
-                tv3.setTextColor(getResources().getColor(R.color.colorAccent));
-                iv3.setVisibility(View.VISIBLE);
+                sunglassesTv.setTextColor(getResources().getColor(R.color.colorwhirt));
+                sunglassesIv.setVisibility(View.VISIBLE);
                 break;
             case "专题分享":
-                tv4.setTextColor(getResources().getColor(R.color.colorAccent));
-                iv4.setVisibility(View.VISIBLE);
+                shareTv.setTextColor(getResources().getColor(R.color.colorwhirt));
+                shareIv.setVisibility(View.VISIBLE);
                 break;
             case "我的购物车":
-                tv5.setTextColor(getResources().getColor(R.color.colorAccent));
-                iv5.setVisibility(View.VISIBLE);
+                shoppingCartTv.setTextColor(getResources().getColor(R.color.colorwhirt));
+                shoppingCartIv.setVisibility(View.VISIBLE);
                 recyclerView.setVisibility(View.INVISIBLE);
                 iv.setVisibility(View.VISIBLE);
                 break;
         }
-//        lv = (ListView) frm_ppp_Lt.findViewById(R.id.ppp_lv);
-//        adapter = new RepeatLvAdapter(context,mydata,R.layout.frm_repeat_item_lv);
-
-
-
-
-//        lv.setDivider(null);
-//        lv.setDividerHeight(0);
-//        lv.setAdapter(adapter);
-//
-//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                if (position==mydata.size()-2){
-//                    controlViewpager.control(0);
-//                }else if (position == mydata.size()-1){
-//                    clickQuitDialog();
-//                }else  {
-//                    controlViewpager.control(position);
-//                }
-//
-//                popupWindow.dismiss();
-//
-//            }
-//        });
-
 
         popupWindow.setContentView(frm_ppp_Lt);
 
@@ -306,7 +270,7 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
 
                 pop_up.setVisibility(View.VISIBLE);
                 recyclerView.setVisibility(View.VISIBLE);
-                frm_ppp_Lt.startAnimation(animation2);
+//                frm_ppp_Lt.startAnimation(animation2);
 
             }
         });
@@ -314,22 +278,7 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
     }
 
 
-    private void initdatas() {
-        mydata = new ArrayList<>();
-//        for (int i = 0; i < 5; i++) {
-//            mydata.add(new MyData(titles));
-        mydata.add(new MyData("浏览所有分类"));
-        mydata.add(new MyData("浏览平光镜"));
-        mydata.add(new MyData("浏览太阳镜"));
-        mydata.add(new MyData("专题分享"));
-        mydata.add(new MyData("我的购物车"));
-        mydata.add(new MyData("返回首页"));
-        mydata.add(new MyData("退出应用程序"));
 
-
-//        }
-
-    }
 
     public void clickQuitDialog() {
 

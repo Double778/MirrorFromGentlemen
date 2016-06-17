5155cbfd67c16bc4f85cab4b838fcbeaae99328e
韩旭 <982810829@qq.com>
2016-06-17 15:42:16 +0800

    改完了

--- MirrorFromGentleman/app/src/main/java/com/zhao/mirrorfromgentleman/ui/fragment/RepeatFragment.java
+++ MirrorFromGentleman/app/src/main/java/com/zhao/mirrorfromgentleman/ui/fragment/RepeatFragment.java
@@ -67,6 +67,10 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
     private PopupWindow popupWindow;
     private List<MyData> mydata;
 
+    private TextView allTv, flatLightTv, sunglassesTv, shareTv, tv5, tv6, tv7;
+
+    private ImageView iv1, iv2, iv3, iv4, iv5;
+
 
     //定义这个内部接口
     private ControlViewpager controlViewpager;
@@ -92,6 +96,8 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
     @Override
     public void initData() {
 
+
+
         //点击出ppp的点击事件
         pop_up.setOnClickListener(this);
 
@@ -102,9 +108,14 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
         if (titles.equals("我的购物车")){
             recyclerView.setVisibility(View.INVISIBLE);
             iv.setVisibility(View.VISIBLE);
+
+
+
         }
 
         initPupop();
+
+
 
         Map<String, String> params = new HashMap<>();
         params.put("token", "0");
@@ -145,10 +156,50 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
 
     @Override
     public void onClick(View v) {
-        popupWindow.showAsDropDown(pop_up);
-        pop_up.setVisibility(View.INVISIBLE);
-        recyclerView.setVisibility(View.INVISIBLE);
+        switch (v.getId()){
+            case R.id.pop_up_Lt:
+                popupWindow.showAsDropDown(pop_up);
+                pop_up.setVisibility(View.INVISIBLE);
+                recyclerView.setVisibility(View.INVISIBLE);
+                break;
+            case R.id.tv1:
+                controlViewpager.control(0);
+                popupWindow.dismiss();
+                break;
+            case R.id.tv2:
+                controlViewpager.control(1);
+                popupWindow.dismiss();
+                break;
+            case R.id.tv3:
+                controlViewpager.control(2);
+                popupWindow.dismiss();
+                break;
+            case R.id.tv4:
+                controlViewpager.control(3);
+                popupWindow.dismiss();
+                break;
+            case R.id.tv5:
+                controlViewpager.control(4);
+                popupWindow.dismiss();
+                break;
+            case R.id.tv6:
+                controlViewpager.control(0);
+                popupWindow.dismiss();
+                break;
+            case R.id.tv7:
+                clickQuitDialog();
+                popupWindow.dismiss();
+                break;
+        }
+
+
+//        popupWindow.showAsDropDown(pop_up);
+//        pop_up.setVisibility(View.INVISIBLE);
+//        recyclerView.setVisibility(View.INVISIBLE);
+
+
     }
+
 
 
     //自定义的接口 来控制Viewpager的位置
@@ -174,31 +225,77 @@ public class RepeatFragment extends BaseFragment implements View.OnClickListener
 
         initdatas();
 
-        lv = (ListView) frm_ppp_Lt.findViewById(R.id.ppp_lv);
-        adapter = new RepeatLvAdapter(context,mydata,R.layout.frm_repeat_item_lv);
+
+        allTv = (TextView) frm_ppp_Lt.findViewById(R.id.all_tv);
+        allTv.setOnClickListener(this);
+        flatLightTv = (TextView) frm_ppp_Lt.findViewById(R.id.flat_light_tv);
+        flatLightTv.setOnClickListener(this);
+        sunglassesTv = (TextView) frm_ppp_Lt.findViewById(R.id.sunglasses_tv);
+        sunglassesTv.setOnClickListener(this);
+        shareTv = (TextView) frm_ppp_Lt.findViewById(R.id.share_tv);
+        shareTv.setOnClickListener(this);
+        tv5 = (TextView) frm_ppp_Lt.findViewById(R.id.tv5);
+        tv5.setOnClickListener(this);
+        tv6 = (TextView) frm_ppp_Lt.findViewById(R.id.tv6);
+        tv6.setOnClickListener(this);
+        tv7 = (TextView) frm_ppp_Lt.findViewById(R.id.tv7);
+        tv7.setOnClickListener(this);
+        iv1 = (ImageView) frm_ppp_Lt.findViewById(R.id.iv1);
+        iv2 = (ImageView) frm_ppp_Lt.findViewById(R.id.iv2);
+        iv3 = (ImageView) frm_ppp_Lt.findViewById(R.id.iv3);
+        iv4 = (ImageView) frm_ppp_Lt.findViewById(R.id.iv4);
+        iv5 = (ImageView) frm_ppp_Lt.findViewById(R.id.iv5);
+
+
+        switch (titles) {
+            case "浏览所有分类":
+                tv1.setTextColor(getResources().getColor(R.color.colorAccent));
+                iv1.setVisibility(View.VISIBLE);
+                break;
+            case "浏览平光镜":
+                tv2.setTextColor(getResources().getColor(R.color.colorAccent));
+                iv2.setVisibility(View.VISIBLE);
+                break;
+            case "浏览太阳镜":
+                tv3.setTextColor(getResources().getColor(R.color.colorAccent));
+                iv3.setVisibility(View.VISIBLE);
+                break;
+            case "专题分享":
+                tv4.setTextColor(getResources().getColor(R.color.colorAccent));
+                iv4.setVisibility(View.VISIBLE);
+                break;
+            case "我的购物车":
+                tv5.setTextColor(getResources().getColor(R.color.colorAccent));
+                iv5.setVisibility(View.VISIBLE);
+                recyclerView.setVisibility(View.INVISIBLE);
+                iv.setVisibility(View.VISIBLE);
+                break;
+        }
+//        lv = (ListView) frm_ppp_Lt.findViewById(R.id.ppp_lv);
+//        adapter = new RepeatLvAdapter(context,mydata,R.layout.frm_repeat_item_lv);
 
 
 
 
-        lv.setDivider(null);
-        lv.setDividerHeight(0);
-        lv.setAdapter(adapter);
-
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position==mydata.size()-2){
-                    controlViewpager.control(0);
-                }else if (position == mydata.size()-1){
-                    clickQuitDialog();
-                }else  {
-                    controlViewpager.control(position);
-                }
-
-                popupWindow.dismiss();
-
-            }
-        });
+//        lv.setDivider(null);
+//        lv.setDividerHeight(0);
+//        lv.setAdapter(adapter);
+//
+//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                if (position==mydata.size()-2){
+//                    controlViewpager.control(0);
+//                }else if (position == mydata.size()-1){
+//                    clickQuitDialog();
+//                }else  {
+//                    controlViewpager.control(position);
+//                }
+//
+//                popupWindow.dismiss();
+//
+//            }
+//        });
 
 
         popupWindow.setContentView(frm_ppp_Lt);
